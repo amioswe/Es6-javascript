3e5794828b60f84b39ae01d23a3c26c604937c19
amioswe <amioswe@gmail.com>
2026-08-12 01:21:21 +0600

    function example create

---
+++ example-es6.js
@@ -0,0 +1,16 @@
+// let name='amiodas';
+// let price=422;
+
+
+
+function finalresult(name,price){
+ let showResult=`Hello , iam ${name}.
+ And my product price are ${price}
+ is more than but
+ your payment are successfully.`
+ console.log(showResult);
+
+return showResult;
+
+}
+console.log(finalresult('Mikhaile',343));
--- template-string.js
+++ template-string.js
@@ -1,5 +1,6 @@
 let name="amiodas";
 let price=599;
+let quantity=6;
 
-let showResult=`Hello ${name}.your product price is ${price}.`
+let showResult=`Hello ${name}.your product price is ${price * quantity}.`
 console.log(showResult);
