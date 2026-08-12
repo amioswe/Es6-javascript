8f74694363dd787676faa84d4e95677fac239ac1
amioswe <amioswe@gmail.com>
2026-08-12 15:07:24 +0600

    arrow-function

---
+++ arrow_function.js
@@ -0,0 +1,13 @@
+// const profile=(name)=>`Hello everyone i am ,${name}das.Good Afternoon.`
+// console.log(profile('amio'));
+
+
+
+const sumNumberbymultipleby5=(a,b)=>{
+    let sum=a+b;
+    let multiple=sum*5;
+    return multiple;
+
+}
+
+console.log(sumNumberbymultipleby5(23,43));
--- spread_rest.js
+++ spread_rest.js
@@ -1,10 +1,26 @@
-let number=[10,32,53,66,77,88,555,4787];
+// let number=[10,32,53,66,77,88,555,4787];
 
-// console.log(...number);
-let maxNumber=Math.max(...number);
+// // console.log(...number);
+// let maxNumber=Math.max(...number);
 
-console.log( 'Maximum numbers are',maxNumber);
+// console.log( 'Maximum numbers are',maxNumber);
 
-let minNumber=Math.min(...number);
+// let minNumber=Math.min(...number);
 
-console.log('Minimum numbers are',minNumber);
+// console.log('Minimum numbers are',minNumber);
+
+
+// let array=[21,42,55,75,66,334];
+
+// let num=array;
+
+// console.log(...num,545,343,343);
+
+
+ const profile = {
+    name:'amiodas',
+    age:23
+
+}
+let showResult={...profile,address:'kaanipur'};
+console.log(showResult);
