c93feba2ff3267e4ec3ac84cd0e5b125b4702969
amioswe <amioswe@gmail.com>
2026-08-12 16:45:05 +0600

    object + function show

--- arrow_function.js
+++ arrow_function.js
@@ -3,11 +3,28 @@
 
 
 
-const sumNumberbymultipleby5=(a,b)=>{
-    let sum=a+b;
-    let multiple=sum*5;
-    return multiple;
+// const sumNumberbymultipleby5= (a,b) =>{
+//     let sum=a+b;
+//     let multiple=sum*5;
+//     return multiple;
 
-}
+// }
 
-console.log(sumNumberbymultipleby5(23,43));
+// console.log(sumNumberbymultipleby5(23,43));
+
+
+
+const student={
+
+    name:'Amiodas',
+    age:23,
+    num:322342,
+    adress:'kanaipur',
+
+    showdetails:function(){
+        return ` My Name is : ${this.name}.And my age is : ${this.age}.`
+
+    }
+};
+
+console.log(student.showdetails());
