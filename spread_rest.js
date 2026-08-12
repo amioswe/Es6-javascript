66a4cce6dbfe881839cee0d5cdb96b5acf2d0ba7
amioswe <amioswe@gmail.com>
2026-08-12 13:59:56 +0600

    maximum and minimum es6

--- spread_rest.js
+++ spread_rest.js
@@ -3,4 +3,8 @@ let number=[10,32,53,66,77,88,555,4787];
 // console.log(...number);
 let maxNumber=Math.max(...number);
 
-console.log(maxNumber);
+console.log( 'Maximum numbers are',maxNumber);
+
+let minNumber=Math.min(...number);
+
+console.log('Minimum numbers are',minNumber);
